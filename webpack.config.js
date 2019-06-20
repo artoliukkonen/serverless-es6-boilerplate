f79a0c6d4e502730964aa2f75e51ea56e2c3fd46
Arto Liukkonen <arto@51.fi>
2019-06-20 12:06:37 +0200

    Update to Node10. Update dependencies & add sourcemap support.

--- webpack.config.js
+++ webpack.config.js
@@ -1,15 +1,33 @@
+const webpack = require('webpack');
 const slsw = require('serverless-webpack');
+const nodeExternals = require('webpack-node-externals');
 
-module.exports = {
-  entry: slsw.lib.entries,
-  target: 'node',
-  mode: slsw.lib.webpack.isLocal ? 'development' : 'production',
-  module: {
-    rules: [{
-      test: /\.js$/,
-      use: ['babel-loader'],
-      include: __dirname,
-      exclude: /node_modules/,
-    }],
-  },
-};
+module.exports = (async () => {
+  const accountId = await slsw.lib.serverless.providers.aws.getAccountId();
+  return {
+    entry: slsw.lib.entries,
+    target: 'node',
+    devtool: 'source- map',
+    externals: [nodeExternals()],
+    mode: slsw.lib.webpack.isLocal ? 'development' : 'production',
+    performance: {
+      hints: false, // Turn off size warnings for entry points
+    },
+    stats: 'minimal', // https://github.com/serverless-heaven/serverless-webpack#stats
+    plugins: [
+      new webpack.DefinePlugin({
+        AWS_ACCOUNT_ID: `${accountId}`,
+      }),
+    ],
+    module: {
+      rules: [
+        {
+          test: /\.js$/,
+          use: ['babel-loader'],
+          include: __dirname,
+          exclude: /node_modules/,
+        },
+      ],
+    },
+  };
+})();
