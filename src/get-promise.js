05752994f5d7fb44e21b6454ab8163c36168b148
Arto Liukkonen <arto@51.fi>
2020-01-16 13:49:25 +0200

    Add travis. Fix lint.

--- src/get-promise.js
+++ src/get-promise.js
@@ -1,7 +1,7 @@
 export default async (event) => {
   console.log('Event', event);
 
-  return new Promise((resolve, reject) => {
+  return new Promise((resolve) => {
     setTimeout(() => resolve({ message: 'Your promise resolved!' }), 500);
   });
 };
